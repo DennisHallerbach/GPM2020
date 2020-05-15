0deeaa002c9916b8f2f756b7f01789c40aec4c21
Dennis Hallerbach <deh6088@thi.de>
2020-05-15 17:07:42 +0200

    First Messaging implementierung

--- messaging/src/main/java/edu/thi/flytransport/messaging/servicetask/GetCredentialsTask.java
+++ messaging/src/main/java/edu/thi/flytransport/messaging/servicetask/GetCredentialsTask.java
@@ -1,5 +1,27 @@
 package edu.thi.flytransport.messaging.servicetask;
 
-public class GetCredentialsTask {
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
+
+import edu.thi.flytransport.messaging.beans.Order;
+
+public class GetCredentialsTask implements JavaDelegate {
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		String Username = (String) execution.getVariable("username");
+		String Password = (String) execution.getVariable("password");
+		Long packetLength = (Long) execution.getVariable("packetLength");
+		Long packetWidth = (Long) execution.getVariable("packetWidth");
+		Long packetHeight = (Long) execution.getVariable("packetHeight");
+		Long packetWeight = (Long) execution.getVariable("packetWeight");
+		
+		Order order = new Order(Username,Password,packetLength,packetWidth,packetHeight,packetWeight);
+		System.out.println(Username + Password);
+		/*execution.setVariable("Order", order);
+		execution.setVariable("gotcredentials", false);*/
+		
+	}
 }
--- messaging/src/main/java/edu/thi/flytransport/messaging/servicetask/NewDroneRequestMessage.java
+++ messaging/src/main/java/edu/thi/flytransport/messaging/servicetask/NewDroneRequestMessage.java
@@ -1,13 +1,39 @@
 package edu.thi.flytransport.messaging.servicetask;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.runtime.Execution;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
+
 
 public class NewDroneRequestMessage implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		// TODO: define message
+		String Username = (String) execution.getVariable("username");
+		String Password = (String) execution.getVariable("password");
+		Long packetLength = (Long) execution.getVariable("packetLength");
+		Long packetWidth = (Long) execution.getVariable("packetWidth");
+		Long packetHeight = (Long) execution.getVariable("packetHeight");
+		Long packetWeight = (Long) execution.getVariable("packetWeight");
+		Map<String, Object> data = new HashMap<String,Object>();
+		data.put("username", Username);
+		data.put("password", Password);
+		data.put("packetLength", packetLength);
+		data.put("packetWidth", packetWidth);
+		data.put("packetHeight", packetHeight);
+		data.put("packetWeight", packetWeight);
+		RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
+        MessageCorrelationResult mcresult = runtimeService.createMessageCorrelation("NewDroneRequestMessage")
+                                                            .setVariables(data)
+                                                            .correlateWithResult();
+        
+        System.out.println(mcresult);
+		
 	}
 
 }
