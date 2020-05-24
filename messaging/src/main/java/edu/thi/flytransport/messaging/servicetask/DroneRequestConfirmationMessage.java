890e7c5681f013a5e77f61ce98d3f8d5eaa5b5a1
Julian Bauer <bauer.jb@gmx.de>
2020-05-24 21:34:10 +0200

    Messaging changes
    - Drone Request Confirmation Message: Added implementation
    - Order Message: edited userProcessId to deliveryProcessId
    - Packet Deposited Message: Added implementation
    - process.bpmn: some changes in messaging tasks

--- messaging/src/main/java/edu/thi/flytransport/messaging/servicetask/DroneRequestConfirmationMessage.java
+++ messaging/src/main/java/edu/thi/flytransport/messaging/servicetask/DroneRequestConfirmationMessage.java
@@ -1,13 +1,24 @@
 package edu.thi.flytransport.messaging.servicetask;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
 
 public class DroneRequestConfirmationMessage implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		// TODO: define message
+		String processId = (String) execution.getVariable("processId");
+		Map<String, Object> data = new HashMap<String,Object>();
+		data.put("deliveryProcessId",processId);
+		RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
+        MessageCorrelationResult mcresult = runtimeService.createMessageCorrelation("DroneRequestConfirmMessage")
+                                                            .setVariables(data)
+                                                            .correlateWithResult();
 	}
 
 }
--- messaging/src/main/java/edu/thi/flytransport/messaging/servicetask/PacketDepositedMessage.java
+++ messaging/src/main/java/edu/thi/flytransport/messaging/servicetask/PacketDepositedMessage.java
@@ -4,13 +4,13 @@ import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-public class OrderMessage implements JavaDelegate {
+public class PacketDepositedMessage implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		String deliveryProcessId = (String) execution.getVariable("deliveryProcessId");
         RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
-        runtimeService.createMessageCorrelation("OrderMessage")
+		runtimeService.createMessageCorrelation("PacketPickedUpMessage")
 				.processInstanceVariableEquals("processId", deliveryProcessId).correlate();
 	}
 
